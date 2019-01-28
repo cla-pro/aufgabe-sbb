318b8ff0f6c713dc5af65b4b369eb27d1de71509
mc-aufgabe4 <->
2019-01-28 15:16:26 +0100

    Adding grouping to einzeltrassen

--- src/main/java/ch/sbb/tms/json/EinzelTrassen.java
+++ src/main/java/ch/sbb/tms/json/EinzelTrassen.java
@@ -1,18 +1,40 @@
 package ch.sbb.tms.json;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.google.gson.Gson;
 
 public class EinzelTrassen {
+
+	@SuppressWarnings("unused")
+	// can be ignored gson neeeds it
 	private Betriebstage betriebstage;
+	// can be ignored gson neeeds it
 	private List<TrassenLauf> trassenlauf;
-	
+
 	public void setBetriebstage(Betriebstage betriebstage) {
 		this.betriebstage = betriebstage;
 	}
+
 	public void setTrassenlauf(List<TrassenLauf> trassenlauf) {
 		this.trassenlauf = trassenlauf;
 	}
-	
-	
-	
+
+	@Override
+	public String toString() {
+
+		if (trassenlauf == null) {
+			return super.toString();
+		}
+
+		Map<String, List<TrassenLauf>> postsPerType = trassenlauf.stream()
+				.collect(Collectors.groupingBy(TrassenLauf::getBetriebspunktId));
+
+		Gson gson = new Gson();
+		String json = gson.toJson(postsPerType);
+		return json;
+	}
+
 }
