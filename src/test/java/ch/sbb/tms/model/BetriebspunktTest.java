843e9adbebe6a999c3d00730e677116a60c77a55
mc-aufgabe5 <->
2019-01-28 11:24:09 +0100

    renamed the app class

--- src/test/java/ch/sbb/tms/model/BetriebspunktTest.java
+++ src/test/java/ch/sbb/tms/model/BetriebspunktTest.java
@@ -1,5 +1,10 @@
 package ch.sbb.tms.model;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ch.sbb.tms.app.GleisBelegungApp;
+
+@SpringBootTest(classes = GleisBelegungApp.class)
 public class BetriebspunktTest {
 
 }
