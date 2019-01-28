78afb0d309484f32b195c7f06cfc3f8a1cf67974
mc-aufgabe5 <->
2019-01-28 11:39:08 +0100

    some junits

--- src/test/java/ch/sbb/tms/model/BetriebspunktTest.java
+++ src/test/java/ch/sbb/tms/model/BetriebspunktTest.java
@@ -1,10 +1,6 @@
 package ch.sbb.tms.model;
 
-import org.springframework.boot.test.context.SpringBootTest;
-
-import ch.sbb.tms.app.GleisBelegungApp;
-
-@SpringBootTest(classes = GleisBelegungApp.class)
 public class BetriebspunktTest {
 
+	
 }
--- src/test/java/ch/sbb/tms/model/DarstellzeitTest.java
+++ src/test/java/ch/sbb/tms/model/DarstellzeitTest.java
@@ -1,6 +1,44 @@
 package ch.sbb.tms.model;
 
+import java.time.Instant;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 
 public class DarstellzeitTest {
+
+	private Darstellzeit obj;
+	private Betriebspunkt betriebspunkt;
+	private Instant von;
+	private Instant bis;
+	
+	@Before
+	public void setup() {
+		obj = new Darstellzeit();		
+		betriebspunkt = new Betriebspunkt();
+		von = Instant.now();
+		bis = Instant.now().plusSeconds(3000);
+	}
+	
+	@Test
+	public void darstellzeit_shouldSetVon() {
+		obj.setVon(von);
+		Assert.assertEquals(von, obj.getVon());
+	}
+	
+	@Test
+	public void darstellzeit_shouldSetBis() {
+		obj.setBis(bis);
+		Assert.assertEquals(bis, obj.getBis());
+	}
+	
+	@Test
+	public void darstellzeit_shouldSetBetriebspunkt() {
+		obj.setBetriebspunkt(betriebspunkt);
+		Assert.assertEquals(betriebspunkt, obj.getBetriebspunkt());
+	}
 
 }
